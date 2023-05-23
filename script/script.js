fd2f259f9c65c0b4c6632c8eb8b532ba9ff76b68
2jdmtec <2jdmtec@bglab.local>
2023-05-23 12:15:26 -0300

    Primeiro commit - Criação do Site, Add do CSS e Add Canvas no JS

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.moveTo(50, 20);
+ctx.bezierCurveTo(40, 70, 50, 70, 80);
+ctx.stroke();          
