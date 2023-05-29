9969d40be4a39b3e1af3bc30d123c98509454899
2jdmtec <2jdmtec@bglab.local>
2023-05-29 10:55:42 -0300

    Segundo Commit - Atualização de arquivos HTML e JS

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
 ctx.beginPath();
-ctx.strokeStyle = "black";
+ctx.strokeStyle() = 'black';
 ctx.moveTo(50, 20);
-ctx.bezierCurveTo(40, 70, 50, 70, 80);
+ctx.bezierCurveTo(40, 70, 50, 90, 80);
 ctx.stroke();          
