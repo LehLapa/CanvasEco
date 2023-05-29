74b8a002e354893d76f9be6e7a1117d001d2cc85
yuritelis <mah.telisgarcia@gmail.com>
2023-05-29 17:29:58 -0300

    Terceiro Commit - Atualização de arquivos HTML e JS

--- script/script.js
+++ script/script.js
@@ -2,7 +2,16 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.beginPath();
-ctx.strokeStyle() = 'black';
-ctx.moveTo(50, 20);
-ctx.bezierCurveTo(40, 70, 50, 90, 80);
-ctx.stroke();          
+ctx.moveTo(20, 90);
+ctx.quadraticCurveTo(60, 100, 100, 90);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(90, 90);
+ctx.quadraticCurveTo(10, 10, 10, 90);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(20, 90);
+ctx.quadraticCurveTo(30, 30, 30, 30);
+ctx.stroke();
